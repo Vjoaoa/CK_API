672f6304d1f9b5fd53658dd635ddb2083522d009
Vjoaoa <89075716+Vjoaoa@users.noreply.github.com>
2022-12-09 19:22:15 -0300

    Provider do thema

--- CTD-FE3-Checkpoint2-Template-main/src/App.jsx
+++ CTD-FE3-Checkpoint2-Template-main/src/App.jsx
@@ -1,11 +1,12 @@
-
-import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
+
 function App() {
+
   return (
-    <>
+    <BrowserRouter>
+      <Navbar/>
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar a classe dark ou light */}
       <div className={`app light}`}>
@@ -15,7 +16,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
--- CTD-FE3-Checkpoint2-Template-main/src/Components/Navbar.jsx
+++ CTD-FE3-Checkpoint2-Template-main/src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
+import { useState,ReactSwitch } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-
+  
   return (
     <header className="sticky-top">
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
@@ -55,14 +56,13 @@ const Navbar = () => {
                  Na linha seguinte deverá ser feito um teste se a aplicação
                  está em dark mode e deverá utilizar o icone ☀ ou 🌙 e btn-dark ou btn-light*/}
                 <button
-                  className={`btn btn-light${styles.btnStyle
-                    }`}
+                  className={`btn btn-light${styles.btnStyle}`} 
                 >
                   ☀ 🌙{" "}
                 </button>
               </li>
             </ul>
-          </div>
+          </div>  
         </div>
       </nav>
     </header>
