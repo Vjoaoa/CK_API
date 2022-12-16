fece7aed1938e33911ee980f4924813ac4a3b2d8
geyson_k <89105671+geysonk@users.noreply.github.com>
2022-12-15 22:28:25 -0300

    Correção rotas

--- CTD-FE3-Checkpoint2-Template-main/src/Routes.jsx
+++ CTD-FE3-Checkpoint2-Template-main/src/Routes.jsx
@@ -11,7 +11,7 @@ const AppRoutes = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/home" element={<Home />} />
-                <Route path="/detail" element={<Detail />} />
+                <Route path="/dentist" element={<Detail />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
